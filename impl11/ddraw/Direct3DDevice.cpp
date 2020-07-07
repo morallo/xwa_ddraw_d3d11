0ff41d71b0529dd6cf8fbb6f33dec86d52576228
morallo <m_orallo@yahoo.es>
2020-07-07 04:29:10 +0200

    SteamVR performance fix, 2nd try

--- impl11/ddraw/Direct3DDevice.cpp
+++ impl11/ddraw/Direct3DDevice.cpp
@@ -6652,7 +6652,12 @@ HRESULT Direct3DDevice::Execute(
 	str << this << " " << __FUNCTION__;
 	LogText(str.str());
 #endif
-
+	// Synchronization point to wait for vsync before we start to send work to the GPU
+	// This avoids blocking the CPU while the compositor waits for the pixel shader effects to run in the GPU
+	// (that's what happens if we sync after Submit+Present)
+	if (g_ExecuteCount == 0) { //only wait once per frame
+		vr::EVRCompositorError error = g_pVRCompositor->WaitGetPoses(&g_rTrackedDevicePose, 0, NULL, 0);
+	}
 	g_ExecuteCount++;
 
 	//log_debug("[DBG] Execute (1)");
