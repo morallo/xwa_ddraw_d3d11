cdb0a0ac70eadc659b6e0f108942e8169f3c1065
Leo Reyes <Leo.H.Reyes@gmail.com>
2020-12-28 23:17:38 -0800

    Fixed laser lights for RebelTurbo and ImpTurbo OPTs
    Turns out I never added the RebelTurbo and ImpTurbo lasers to the laser light
    effects. These OPTs also need material files.
    
    Also adding material files for ImpTurbo* and RebelTurbo* lasers.
    
    Manually cherry-picked from: 12f543daeb00b3b9ac8ca1446324cf7baba7b94f

--- impl11/ddraw/Direct3DDevice.cpp
+++ impl11/ddraw/Direct3DDevice.cpp
@@ -3065,7 +3065,7 @@ HRESULT Direct3DDevice::Execute(
 						}
 					}
 
-					bIsLaser = lastTextureSelected->is_Laser;
+					bIsLaser = lastTextureSelected->is_Laser || lastTextureSelected->is_TurboLaser;
 					bIsLightTexture = lastTextureSelected->is_LightTexture;
 					bIsText = lastTextureSelected->is_Text;
 					bIsReticle = lastTextureSelected->is_Reticle;
@@ -4843,7 +4843,7 @@ HRESULT Direct3DDevice::Execute(
 				// Apply BLOOM flags and 32-bit mode enhancements
 				if (bLastTextureSelectedNotNULL)
 				{
-					if (lastTextureSelected->is_Laser || lastTextureSelected->is_TurboLaser) {
+					if (bIsLaser) {
 						bModifiedShaders = true;
 						g_PSCBuffer.fBloomStrength = lastTextureSelected->is_Laser ?
 							g_BloomConfig.fLasersStrength : g_BloomConfig.fTurboLasersStrength;
