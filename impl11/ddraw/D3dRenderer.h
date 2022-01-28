6ee8df7183066a61e43ccf19f6f8055ccf00e3a4
Leo Reyes <Leo.H.Reyes@gmail.com>
2022-01-27 23:20:59 -0800

    Code refactor to make the VR implementation (and others) easier and cleaner.
    No new code here, just moved stuff around to put the EffectsRenderer in its own
    file. Other implementations can now be made on their own files as well.

---
+++ impl11/ddraw/D3dRenderer.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <map>
+
+#pragma pack(push, 1)
+
+struct D3dGlobalLight
+{
+	float direction[4];
+	float color[4];
+};
+
+struct D3dLocalLight
+{
+	float position[4];
+	float color[4];
+};
+
+struct D3dConstants
+{
+	float viewportScale[4];
+	float projectionValue1;
+	float projectionValue2;
+	float projectionDeltaX;
+	float projectionDeltaY;
+	float floorLevel;
+	float cameraPositionX;
+	float cameraPositionY;
+	float hangarShadowAccStartEnd;
+	float s_V0x0662814;
+	float s_V0x064D1A4;
+	float s_V0x06626F4;
+	float s_V0x063D194;
+	float transformView[16];
+	float transformWorldView[16];
+	int globalLightsCount;
+	int localLightsCount;
+	int renderType;
+	int renderTypeIllum;
+	D3dGlobalLight globalLights[8];
+	D3dLocalLight localLights[8];
+};
+
+static_assert(sizeof(D3dConstants) % 16 == 0, "D3dConstants size must be multiple of 16");
+
+struct D3dVertex
+{
+	int iV;
+	int iN;
+	int iT;
+	int c;
+};
+
+struct D3dTriangle
+{
+	int v1;
+	int v2;
+	int v3;
+};
+
+#pragma pack(pop)
+
+struct DrawCommand {
+	Direct3DTexture *colortex, *lighttex;
+	ID3D11ShaderResourceView *vertexSRV, *normalsSRV, *texturesSRV;
+	ID3D11Buffer *vertexBuffer, *indexBuffer;
+	int trianglesCount;
+	D3dConstants constants;
+	// Extra fields needed for transparent layers
+	ID3D11ShaderResourceView *SRVs[2];
+	PixelShaderCBuffer PSCBuffer;
+	DCPixelShaderCBuffer DCPSCBuffer;
+	bool bIsCockpit, bIsGunner, bIsBlastMark;
+	ComPtr<ID3D11PixelShader> pixelShader;
+};
+
+class D3dRenderer
+{
+public:
+	D3dRenderer();
+	virtual void SceneBegin(DeviceResources* deviceResources);
+	virtual void SceneEnd();
+	void FlightStart();
+	virtual void MainSceneHook(const SceneCompData* scene);
+	void HangarShadowSceneHook(const SceneCompData* scene);
+	virtual void UpdateTextures(const SceneCompData* scene);
+	void UpdateMeshBuffers(const SceneCompData* scene);
+	void ResizeDataVector(const SceneCompData* scene);
+	void CreateDataScene(const SceneCompData* scene);
+	void UpdateVertexAndIndexBuffers(const SceneCompData* scene);
+	void UpdateConstantBuffer(const SceneCompData* scene);
+	virtual void RenderScene();
+	virtual void RenderDeferredDrawCalls();
+	void BuildGlowMarks(SceneCompData* scene);
+	void RenderGlowMarks();
+	void Initialize();
+	void CreateConstantBuffer();
+	void CreateStates();
+	void CreateShaders();
+	void GetViewport(D3D11_VIEWPORT* viewport);
+	void GetViewportScale(float* viewportScale);
+
+protected:
+	DeviceResources* _deviceResources;
+	ID3D11Buffer *_lastVertexBuffer, *_lastIndexBuffer;
+
+	int _totalVerticesCount;
+	int _totalTrianglesCount;
+
+	int _currentFaceIndex;
+
+	int _verticesCount;
+	std::vector<D3dVertex> _vertices;
+	int _trianglesCount;
+	std::vector<D3dTriangle> _triangles;
+
+	std::vector<XwaD3dVertex> _glowMarksVertices;
+	std::vector<XwaD3dTriangle> _glowMarksTriangles;
+
+	bool _isInitialized;
+	UINT _meshBufferInitialCount;
+	std::map<int, ComPtr<ID3D11ShaderResourceView>> _meshVerticesViews;
+	std::map<int, ComPtr<ID3D11ShaderResourceView>> _meshNormalsViews;
+	std::map<int, ComPtr<ID3D11ShaderResourceView>> _meshTextureCoordsViews;
+	XwaVector3* _lastMeshVertices;
+	ID3D11ShaderResourceView* _lastMeshVerticesView;
+	XwaVector3* _lastMeshVertexNormals;
+	ID3D11ShaderResourceView* _lastMeshVertexNormalsView;
+	XwaTextureVertex* _lastMeshTextureVertices;
+	ID3D11ShaderResourceView* _lastMeshTextureVerticesView;
+	std::map<int, ComPtr<ID3D11Buffer>> _vertexBuffers;
+	std::map<int, ComPtr<ID3D11Buffer>> _triangleBuffers;
+	std::map<int, std::pair<int, int>> _vertexCounters;
+	D3dConstants _constants;
+	ComPtr<ID3D11Buffer> _constantBuffer;
+	ComPtr<ID3D11RasterizerState> _rasterizerState;
+	ComPtr<ID3D11RasterizerState> _rasterizerStateWireframe;
+	ComPtr<ID3D11SamplerState> _samplerState;
+	ComPtr<ID3D11BlendState> _solidBlendState;
+	ComPtr<ID3D11DepthStencilState> _solidDepthState;
+	ComPtr<ID3D11BlendState> _transparentBlendState;
+	ComPtr<ID3D11DepthStencilState> _transparentDepthState;
+	ComPtr<ID3D11InputLayout> _inputLayout;
+	ComPtr<ID3D11VertexShader> _vertexShader;
+	ComPtr<ID3D11PixelShader> _pixelShader;
+	ComPtr<ID3D11VertexShader> _shadowVertexShader;
+	ComPtr<ID3D11PixelShader> _shadowPixelShader;
+	D3D11_VIEWPORT _viewport;
+};
+
+extern bool g_isInRenderLasers;
+// The following flag cannot be used reliably to tell when the miniature is being rendered: it's
+// false if we're in External Camera mode even if a craft is targeted.
+extern bool g_isInRenderMiniature;
+extern bool g_isInRenderHyperspaceLines;
+
+extern D3dRenderer g_xwa_d3d_renderer;
---
+++ impl11/ddraw/EffectsRenderer.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <vector>
+#include <ScreenGrab.h>
+#include <wincodec.h>
+
+#include "globals.h"
+#include "common.h"
+#include "commonVR.h"
+
+#include "XwaD3dRendererHook.h"
+#include "D3dRenderer.h"
+#include "DeviceResources.h"
+#include "Direct3DTexture.h"
+#include "Matrices.h"
+#include "joystick.h"
+
+// ****************************************************
+// External variables
+// ****************************************************
+extern bool g_bIsTargetHighlighted, g_bPrevIsTargetHighlighted, g_bAOEnabled;
+extern bool g_bExplosionsDisplayedOnCurrentFrame, g_bEnableVR, g_bProceduralLava;
+extern bool g_bIsScaleableGUIElem, g_bScaleableHUDStarted, g_bIsTrianglePointer;
+extern float g_fSSAOAlphaOfs;
+
+// ****************************************************
+// Debug variables
+// ****************************************************
+extern bool g_bDumpSSAOBuffers;
+extern int g_iD3DExecuteCounter, g_iD3DExecuteCounterSkipHi, g_iD3DExecuteCounterSkipLo;
+
+// ****************************************************
+// Temporary, we may not need these here
+// ****************************************************
+extern bool g_bPrevIsScaleableGUIElem, g_bStartedGUI;
+extern bool g_bIsFloating3DObject, g_bPrevIsFloatingGUI3DObject;
+extern bool g_bTargetCompDrawn;
+extern bool g_bEnableAnimations;
+extern HyperspacePhaseEnum g_HyperspacePhaseFSM;
+
+bool IsInsideTriangle(Vector2 P, Vector2 A, Vector2 B, Vector2 C, float *u, float *v);
+Matrix4 ComputeLightViewMatrix(int idx, Matrix4 &Heading, bool invert);
+
+class EffectsRenderer : public D3dRenderer
+{
+private:
+	bool _bLastTextureSelectedNotNULL, _bLastLightmapSelectedNotNULL, _bIsLaser, _bIsCockpit;
+	bool _bIsGunner, _bIsExplosion, _bIsBlastMark, _bHasMaterial, _bDCIsTransparent, _bDCElemAlwaysVisible;
+	bool _bModifiedShaders, _bModifiedPixelShader, _bModifiedBlendState, _bModifiedSamplerState;
+	bool _bIsNoisyHolo, _bWarheadLocked, _bIsTargetHighlighted, _bIsHologram, _bRenderingLightingEffect;
+	bool _bCockpitConstantsCaptured, _bExternalCamera, _bCockpitDisplayed, _bIsTransparentCall;
+	bool _bShadowsRenderedInCurrentFrame, _bJoystickTransformReady;
+	D3dConstants _CockpitConstants;
+	XwaTransform _CockpitWorldView;
+	Direct3DTexture *_lastTextureSelected = nullptr;
+	Direct3DTexture *_lastLightmapSelected = nullptr;
+	std::vector<DrawCommand> _LaserDrawCommands;
+	std::vector<DrawCommand> _TransparentDrawCommands;
+	Matrix4 _joystickMeshTransform;
+
+	VertexShaderCBuffer _oldVSCBuffer;
+	PixelShaderCBuffer _oldPSCBuffer;
+	DCPixelShaderCBuffer _oldDCPSCBuffer;
+	ComPtr<ID3D11Buffer> _oldVSConstantBuffer;
+	ComPtr<ID3D11Buffer> _oldPSConstantBuffer;
+	ComPtr<ID3D11ShaderResourceView> _oldVSSRV[3];
+	ComPtr<ID3D11ShaderResourceView> _oldPSSRV[13];
+	ComPtr<ID3D11VertexShader> _oldVertexShader;
+	ComPtr<ID3D11PixelShader> _oldPixelShader;
+	ComPtr<ID3D11SamplerState> _oldPSSamplers[2];
+	ComPtr<ID3D11RenderTargetView> _oldRTVs[8];
+	ComPtr<ID3D11DepthStencilView> _oldDSV;
+	ComPtr<ID3D11DepthStencilState> _oldDepthStencilState;
+	ComPtr<ID3D11BlendState> _oldBlendState;
+	ComPtr<ID3D11InputLayout> _oldInputLayout;
+	ComPtr<ID3D11Buffer> _oldVertexBuffer, _oldIndexBuffer;
+	D3D11_PRIMITIVE_TOPOLOGY _oldTopology;
+	UINT _oldStencilRef, _oldSampleMask;
+	FLOAT _oldBlendFactor[4];
+	UINT _oldStride, _oldOffset, _oldIOffset;
+	DXGI_FORMAT _oldFormat;
+	D3D11_VIEWPORT _oldViewports[2];
+	UINT _oldNumViewports = 2;
+
+	HRESULT QuickSetZWriteEnabled(BOOL Enabled);
+	void EnableTransparency();
+	void EnableHoloTransparency();
+	inline ID3D11RenderTargetView *SelectOffscreenBuffer(bool bIsMaskable, bool bSteamVRRightEye);
+	Matrix4 GetShadowMapLimits(Matrix4 L, float *OBJrange, float *OBJminZ);
+
+	void SaveContext();
+	void RestoreContext();
+
+public:
+	EffectsRenderer();
+	virtual void SceneBegin(DeviceResources* deviceResources);
+	virtual void SceneEnd();
+	virtual void MainSceneHook(const SceneCompData* scene);
+	virtual void RenderScene();
+	virtual void UpdateTextures(const SceneCompData* scene);
+	// State Management
+	void DoStateManagement(const SceneCompData* scene);
+	void ApplyMaterialProperties();
+	void ApplySpecialMaterials();
+	void ApplyBloomSettings();
+	void DCCaptureMiniature();
+	// Returns true if the current draw call needs to be skipped
+	bool DCReplaceTextures();
+	virtual void ExtraPreprocessing();
+	void AddLaserLights(const SceneCompData* scene);
+	void ApplyProceduralLava();
+	void ApplyGreebles();
+	void ApplyAnimatedTextures();
+
+	// Deferred rendering
+	void RenderLasers();
+	void RenderTransparency();
+	void RenderShadowMap();
+	void RenderDeferredDrawCalls();
+};
+
+extern EffectsRenderer g_effects_renderer;
+
+extern D3dRenderer &g_current_renderer;
