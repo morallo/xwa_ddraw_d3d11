43480ca522bfa7ef75d22b295e701031a6ab1f5c
Leo Reyes <Leo.H.Reyes@gmail.com>
2023-08-17 20:53:08 -0700

    Bump to version 1.0.3. Add ReadFlippedDATImageData for legacy DAT files.
    Legacy DAT files still need to flip images and swizzle their channels, so let's
    add ReadFlippedDATImageData() to support them. The decision on which function to
    use is left to ddraw.

--- impl11/DATReader/Main.cs
+++ impl11/DATReader/Main.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 /*
@@ -39,6 +40,18 @@ namespace DATReader
         static DatImage m_DATImage = null;
         // Enable verbosity
         static bool m_Verbose = false;
+
+        [DllExport(CallingConvention.Cdecl)]
+        public static uint GetDATReaderVersion()
+        {
+            Version version = typeof(Main).Assembly.GetName().Version;
+            //Trace.WriteLine("[DBG] [C#] DATReader version: " + version.ToString());
+            //Trace.WriteLine("[DBG] [C#] Major: " + version.Major);
+            //Trace.WriteLine("[DBG] [C#] Minor: " + version.Minor);
+            //Trace.WriteLine("[DBG] [C#] Build: " + version.Build);
+            int V = (version.Major << 16) | (version.Minor << 8) | version.Build;
+            return (uint)V;
+        }
 
         [DllExport(CallingConvention.Cdecl)]
         public static void SetDATVerbosity(bool Verbose)
@@ -163,6 +176,80 @@ namespace DATReader
             }
 
             Marshal.Copy(data, 0, new IntPtr(RawData_out), data.Length);
+            return true;
+        }
+
+        // Legacy function. Deprecated. To be used only with old DAT files.
+        [DllExport(CallingConvention.Cdecl)]
+        public static unsafe bool ReadFlippedDATImageData(byte* RawData_out, int RawData_size)
+        {
+            if (m_DATImage == null || m_DATFile == null)
+            {
+                Trace.WriteLine("[DBG] [C#] Must cache an image first");
+                return false;
+            }
+
+            //m_DATImage = DatFile.GetImageDataById(m_DATFile.FileName, m_DATImage.GroupId, m_DATImage.ImageId);
+
+            if (!m_DATFile.HasImagesData)
+            {
+                m_DATFile = DatFile.FromFile(m_DATFile.FileName, true);
+                m_DATImage = m_DATFile.GetImageById(m_DATImage.GroupId, m_DATImage.ImageId);
+            }
+
+            //m_DATImage.ConvertToFormat25(); // Looks like there's no need to do any conversion
+            short W = m_DATImage.Width;
+            short H = m_DATImage.Height;
+
+            if (RawData_size == W * H)
+            {
+                Marshal.Copy(m_DATImage.GetRawData(), 0, new IntPtr(RawData_out), m_DATImage.GetRawData().Length);
+                return true;
+            }
+
+            byte[] data = m_DATImage.Format == DatImageFormat.Format25 ? m_DATImage.GetRawData() : m_DATImage.GetImageData();
+
+            int len = data.Length;
+            if (m_Verbose)
+                Trace.WriteLine("[DBG] [C#] RawData, W*H*4 = " + (W * H * 4) + ", len: " + len + ", Format: " + m_DATImage.Format);
+
+            if (RawData_out == null)
+            {
+                Trace.WriteLine("[DBG] [C#] ReadDATImageData: output buffer should not be NULL");
+                return false;
+            }
+
+            try
+            {
+                int min_len = RawData_size;
+                if (data.Length < min_len) min_len = data.Length;
+                // For some reason, the images are still upside down when used as SRVs
+                // So, let's flip them here. RowOfs and RowStride are used to flip the
+                // image by reading it "backwards".
+                UInt32 OfsOut = 0, OfsIn = 0, RowStride = (UInt32)W * 4, RowOfs = (UInt32)(H - 1) * RowStride;
+                for (int y = 0; y < H; y++)
+                {
+                    OfsIn = RowOfs; // Flip the image
+                    for (int x = 0; x < W; x++)
+                    {
+                        RawData_out[OfsOut + 2] = data[OfsIn + 0]; // B
+                        RawData_out[OfsOut + 1] = data[OfsIn + 1]; // G
+                        RawData_out[OfsOut + 0] = data[OfsIn + 2]; // R
+                        RawData_out[OfsOut + 3] = data[OfsIn + 3]; // A
+                        OfsIn += 4;
+                        OfsOut += 4;
+                    }
+                    // Flip the image and prevent underflows:
+                    RowOfs -= RowStride;
+                    if (RowOfs < 0) RowOfs = 0;
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine("[DBG] [C#] Exception: " + e + ", caught in ReadDATImageData");
+                return false;
+            }
+
             return true;
         }
 
